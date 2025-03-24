f0fd5abc108c2a505d16eb5ce7f221cd1518e8d7
Pierre-MarieMarchio <pierremariemarchio.pro@gmail.com>
2025-03-24 19:15:18 +0100

    changed spinner timer

--- src/app/components/blague-banner/blague-banner.component.tsx
+++ src/app/components/blague-banner/blague-banner.component.tsx
@@ -1,17 +1,11 @@
+import { useEffect, useState } from "react";
 import { useFetch } from "../../hooks/useFetch";
 import { Blague } from "../../interfaces/Blague.interface";
 import "./presentation-banner.css";
 import carambarImgBanner from "/banner.jpg";
 
 export const BlagueBanner = () => {
-  const { error, loading, data, fetchData, resetData } = useFetch<Blague>(
-    "https://projet-de-selection-simplon-back-1.onrender.com/api/V1/blagues/random"
-  );
-
-  const handleClick = () => {
-    resetData();
-    fetchData();
-  };
+  const { error, loading, data, refreshBlague } = useBlague();
 
   return (
     <section className=" container direction-col blague-banner-container">
@@ -22,7 +16,7 @@ export const BlagueBanner = () => {
           <h2 className="title-red blague-title-buton">c'est de la Barre ! </h2>
           <button
             className="blague-button title-black"
-            onClick={() => handleClick()}
+            onClick={() => refreshBlague()}
             disabled={loading}
           >
             Rire garanti !
@@ -41,13 +35,13 @@ export const BlagueBanner = () => {
               </p>
             </div>
           )}
-          {!error && data && (
+          {!error && !loading && data && (
             <div className="container">
               <p className="text-primary-imp m-1">{data.question}</p>
               <p className="text-primary">{data.response}</p>
             </div>
           )}
-          {!error && !data && (
+          {!error && !loading && !data && (
             <div className="container">
               <p className="text-primary-imp m-1">
                 Qui est jaune, drôle et complètement barré ?
@@ -61,4 +55,29 @@ export const BlagueBanner = () => {
       </div>
     </section>
   );
+};
+
+const useBlague = () => {
+  const { error, loading, data, fetchData, resetData } = useFetch<Blague>(
+    "https://projet-de-selection-simplon-back-1.onrender.com/api/V1/blagues/random"
+  );
+
+  const [delayedLoading, setDelayedLoading] = useState(true);
+
+  useEffect(() => {
+    if (!loading) {
+      const timer = setTimeout(() => setDelayedLoading(false), 1000);
+      return () => clearTimeout(timer);
+    } else {
+      setDelayedLoading(true);
+    }
+  }, [loading]);
+
+  const refreshBlague = () => {
+    resetData();
+    fetchData();
+    setDelayedLoading(true);
+  };
+
+  return { error, loading: delayedLoading, data, refreshBlague };
 };
